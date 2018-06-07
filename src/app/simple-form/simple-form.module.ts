c590e452979a082d8c9e8a1e2fc6cd0e686e9cd1
Andrew Listopadov <andrew.listopadov@gmail.com>
2018-06-08 00:34:36 +0300

    removes extra dependency;

--- src/app/simple-form/simple-form.module.ts
+++ src/app/simple-form/simple-form.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SimpleFormComponent } from './simple-form.component';
 
 @NgModule({
   imports: [
-    // CommonModule,
     FormsModule
   ],
   declarations: [SimpleFormComponent],
