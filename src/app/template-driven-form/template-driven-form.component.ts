b6c969b1fdf28d6af233b6ed32b37872a13185ef
andrewlistopadov <andrew.listopadov@gmail.com>
2018-06-12 02:18:10 +0300

    makes reactive and template driven forms functionally equivalent;

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+export class User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+
+  constructor(firstName = '', lastName = '', email = '', phoneNumber = '') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+  }
+}
 
 @Component({
   selector: 'template-driven-form',
@@ -6,14 +21,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  user: User;
 
   constructor() { }
 
   ngOnInit() {
+    this.user = new User('Andrei', '', 'andrew.listopadov@gmail.com', '+79998887766');
   }
 
-  onSubmit(form: any): void {
-    alert(`first name: ${form.firstName}\nlastname: ${form.lastName}\nemail: ${form.email}`);
+  onSubmit(user: User): void {
+    alert(`first name: ${user.firstName}\nlastname: ${user.lastName}\nemail: ${user.email}\nphone: ${user.phoneNumber}`);
+  }
+
+  controlIsInvalid(control: FormControl): boolean | null {
+    return control && control.invalid && control.touched ? true : null;
+  }
+
+  controlHasError(control: FormControl, error: string): boolean | null {
+    return control && control.errors && control.errors[error] ? true : null;
   }
 
 }
--- src/app/template-driven-form/template-driven-form.module.ts
+++ src/app/template-driven-form/template-driven-form.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TemplateDrivenFormComponent } from './template-driven-form.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule
   ],
   declarations: [TemplateDrivenFormComponent],
